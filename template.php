0d2f426ebfbb394925dcc4cfee17b99421dd5545
IrinaAlekseevna <irina.fam.5@yandex.ru>
2021-04-07 09:47:33 +0300

    Create template.php

---
+++ template.php
@@ -0,0 +1,18 @@
+<div class="large-2 medium-3 cell route">
+			<div class="data-time data-time-rangeslider">
+				<div class="rangeslider__holder">
+					<input class="rangeslider__output recalc std-height" name="timefrom" id="timepicker" type="text" value="10:00" />
+					<input type="range" min="0" max="2350" step="10" value="1000" id="loanAmount">
+				</div>
+			</div>
+        </div>
+
+
+        <div class="large-2 medium-3 cell route">
+        	<div class="data-time data-time-rangeslider">
+				<div class="rangeslider__holder">
+					<input class="rangeslider__output recalc std-height" id="timepicker2" name="timeto" type="text" value="15:00" />
+					<input type="range" min="0" max="2350" step="10" value="1500" id="loanTerm">
+				</div>
+			</div>
+        </div>
